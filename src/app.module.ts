ef76e8f8e7778f2b91fdd96b07f476cd102b087e
Andrey Bashun <a.a.bashun@gmail.com>
2022-06-19 00:32:08 +0300

    19.06.22-3

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,13 @@
 import {Module} from "@nestjs/common";
 import { DocumentModule } from './document/document.module';
-import {MongooseModule} from "@nestjs/mongoose";
+//import {MongooseModule} from "@nestjs/mongoose";
 
 
 @Module({
 
 
   imports: [
-      MongooseModule.forRoot('mongodb://localhost:27017/mrb-info?readPreference=primary&directConnection=true&ssl=false'),
+      //MongooseModule.forRoot('mongodb://localhost:27017/mrb-info?readPreference=primary&directConnection=true&ssl=false'),
       DocumentModule]
 })
 export class AppModule{}
