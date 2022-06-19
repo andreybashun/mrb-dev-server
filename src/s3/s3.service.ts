2090b8698f3b93924590693364b312f7aaaf0f99
Andrey Bashun <a.a.bashun@gmail.com>
2022-06-20 00:34:45 +0300

    19.06.22-3

---
+++ src/s3/s3.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@nestjs/common';
+import { S3 } from 'aws-sdk';
+
+@Injectable()
+export class S3Service {
+
+    async upload(file) {
+        const { originalname } = file;
+        const bucketS3 = 'mrb-doc';
+        await this.uploadS3(file.buffer, bucketS3, originalname);
+    }
+
+    async uploadS3(file, bucket, name) {
+        const s3 = this.getS3();
+        const params = {
+            Bucket: bucket,
+            Key: String(name),
+            Body: file,
+        };
+        return new Promise((resolve, reject) => {
+            let s3Response = s3.upload(params, (err, data) => {
+                if (err) {
+                    console.log(err);
+                    reject(err.message);
+                }
+                resolve(data);
+                console.log(s3Response);
+            });
+        });
+    }
+
+    getS3() {
+        return new S3({
+            accessKeyId: 'YCAJEgPeLLENuh0iiocABjh5q',
+            secretAccessKey: 'YCO3UkhdeDbzqbvDz6BptW6UgzjqmJSb_Q3U1Xrr',
+            endpoint: 'storage.yandexcloud.net',
+            s3ForcePathStyle: true,
+            signatureVersion: 'v4',
+            region: 'ru-central1'
+        });
+    }
+}
